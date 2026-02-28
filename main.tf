02e7812a7362b18df9f1fcb2107ce574e46927fa
Arpit Patel <pate1562@algonquinlive.com>
2026-02-28 01:41:14 -0500

    add public IP

--- main.tf
+++ main.tf
@@ -29,3 +29,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.labelPrefix}-A05-RG"
   location = var.region
 }
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.labelPrefix}-A05-PIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
