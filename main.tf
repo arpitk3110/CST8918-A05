28ead317b17806e9834105ee525e92ec57953c6c
Arpit Patel <pate1562@algonquinlive.com>
2026-02-28 02:22:22 -0500

    add cloud-init script

--- main.tf
+++ main.tf
@@ -102,3 +102,14 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+data "cloudinit_config" "init" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "init.sh"
+    content_type = "text/x-shellscript"
+    content      = file("${path.module}/init.sh")
+  }
+}
